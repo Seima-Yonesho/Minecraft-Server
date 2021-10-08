46fc3ce47eb071a19d54f67c2949d27d0f16e573
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2021-10-08 20:05:11 +0900

    git rm file

--- run.bat
+++
@@ -1,5 +0,0 @@
-REM Forge requires a configured set of both JVM and program arguments.
-REM Add custom JVM arguments to the user_jvm_args.txt
-REM Add custom program arguments {such as nogui} to this file at the end of the next line
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.17.1-37.0.53/win_args.txt nogui
-pause
